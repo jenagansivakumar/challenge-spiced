2ee72e82dd5abce099e9fb96b74c04641b2dc35f
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-03-20 13:49:12 +0100

    finished converting booleans to string

---
+++ javascript/js_conditions_and booleans/booleanToString.js
@@ -0,0 +1,15 @@
+// Part 1
+// Declare a variable name input and assign a boolean value and a variable name result. Write an if statement, that takes our input value and assign to result a string "Yes" if input is true, or a "No" if input is false. Console Log result
+
+// Part 2
+// Redo part 1 but using the ternary operator instead of the if statement
+
+const input = true;
+let result;
+
+if (input == true) {
+  result = "Yes";
+} else {
+  result = "No";
+}
+console.log(result);
