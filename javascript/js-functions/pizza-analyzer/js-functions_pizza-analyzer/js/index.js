a1ed9a2fcd7ea70cf40cc9ce804541f238ce711e
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-03-22 09:57:02 +0100

    nearly done with challenge

--- javascript/js-functions/pizza-analyzer/js-functions_pizza-analyzer/js/index.js
+++ javascript/js-functions/pizza-analyzer/js-functions_pizza-analyzer/js/index.js
@@ -7,20 +7,32 @@ const pizza2 = document.querySelector('[data-js="pizza-2"]');
 const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
+let pizzaSize1 = pizzaInput1.value;
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  pizzaSize1 = pizzaInput1.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
+// let pizzaSize1 = 24;
 
+let pizzaSize2 = pizzaInput2.value;
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  let pizzaSize1 = 24;
 });
 
 // Task 1
 
 function calculatePizzaGain(diameter1, diameter2) {
-  // write your code here
+  let areaOfPizza1 = Math.PI * (diameter1 / 2) ** 2;
+  let areaOfPizza2 = Math.PI * (diameter2 / 2) ** 2;
+  let pizzaGain = ((areaOfPizza2 - areaOfPizza1) / areaOfPizza1) * 100;
+  output.textContent = Math.round(pizzaGain);
 }
+
+calculatePizzaGain(10, 20);
+// console.log(calculatePizzaGain(5, 10));
 
 // Task 2
 
