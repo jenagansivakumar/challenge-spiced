31a158b59d957e3f969f5e8d978724b59c72d49c
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-03-24 16:36:11 +0100

    finished uppercase

--- javascript/controlled-input-uppercase/js-forms-2_controlled-input-uppercase/js/index.js
+++ javascript/controlled-input-uppercase/js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,8 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+input.addEventListener("input", function (e) {
+  const upperCase = e.target.value;
+  e.target.value = value.toUpperCase();
+});
