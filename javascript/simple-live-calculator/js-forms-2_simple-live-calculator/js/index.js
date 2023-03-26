50486643366ec2dffc0b2ab5f07ca01a247924ac
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-03-26 18:36:17 +0200

    finished calculator challenge

--- javascript/simple-live-calculator/js-forms-2_simple-live-calculator/js/index.js
+++ javascript/simple-live-calculator/js-forms-2_simple-live-calculator/js/index.js
@@ -4,4 +4,10 @@ const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
 
-firstInput.addEventListener("input", function (e) {});
+firstInput.addEventListener("input", function (e) {
+  result.textContent = firstInput.value * secondInput.value;
+});
+
+secondInput.addEventListener("input", function (e) {
+  result.textContent = secondInput.value * firstInput.value;
+});
