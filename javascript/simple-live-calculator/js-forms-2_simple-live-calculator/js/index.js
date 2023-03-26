ea15c5a7da2af0561606f7b210949bbc23c3bd67
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-03-26 12:09:01 +0200

    1st attempt

--- javascript/simple-live-calculator/js-forms-2_simple-live-calculator/js/index.js
+++ javascript/simple-live-calculator/js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,5 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+firstInput.addEventListener("input", function (e) {});
