a7ea7d1a6e55b900fb7126b53c96dd2c6fc494a4
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-03-30 17:11:27 +0200

    finished

--- javascript/js_structure/spacerocket/js-structure_spacerocket/js/payload/satellites.js
+++ javascript/js_structure/spacerocket/js-structure_spacerocket/js/payload/satellites.js
@@ -4,12 +4,12 @@ export const NFSAT = {
   id: "nfsat01",
   type: "NFSAT",
   [checksum]: "df4eb1ac1f0d77a50fc5bfc3c34c64965ace6907",
-  cost: 2500000,
+  cost: 25000000000,
 };
 
 export const FISHSAT = {
   id: "fishsat42",
   type: "FISHSAT",
   [checksum]: "c69fe3789cc3761e3ef2fb593d6ac1d1937773bc",
-  cost: 4000000,
+  cost: 4000000000,
 };
