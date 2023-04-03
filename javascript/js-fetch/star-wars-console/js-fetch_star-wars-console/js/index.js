3d07d71aa067dcbc1d4655490c0b1e5e501d80e4
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-04-03 16:29:25 +0200

    finished first challenge

--- javascript/js-fetch/star-wars-console/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch/star-wars-console/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,25 @@
-console.clear();
+// console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      console.log("bad request");
+    } else {
+      const starWarsData = await response.json();
+      // log of the whole object
+      console.log("starWarsData ", starWarsData);
+      console.log(starWarsData.results[4].name, starWarsData.results[4].height);
+      console.log(starWarsData.results[2].name, starWarsData.results[2].height);
+
+      console.log(starWarsData.results[2].eye_color);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 fetchData();
