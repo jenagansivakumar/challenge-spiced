2f771c1b6661ba7750285e50c39f3874f25eed64
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-04-05 11:45:05 +0200

    finished

--- javascript/js-modern-syntax/destructuring-objects/js-modern-syntax_destructuring-objects/index.js
+++ javascript/js-modern-syntax/destructuring-objects/js-modern-syntax_destructuring-objects/index.js
@@ -1,35 +1,48 @@
-console.clear();
+// console.clear();
 
-// EXERCISE 1
-// Extract only the course name from the company object.
-// Make sure to export your variable as in the example below to make the tests work.
+// // EXERCISE 1
+// // Extract only the course name from the company object.
+// // Make sure to export your variable as in the example below to make the tests work.
 
-const company = {
-  name: "neuefische",
-  course: "Web Development",
-  location: "Hamburg",
-};
+// const company = {
+//   name: "neuefische",
+//   course: "Web Development",
+//   location: "Hamburg",
+// };
 
-// example: export const {value1} = myObject;
+// const { name, course, location } = company;
+// console.log(course);
 
-// EXERCISE 2
-// We have an object called 'user'.
-// Write the destructuring assignment that reads:
-//  2.1: 'name' property into the variable 'name'.
-//  2.2: 'years' property into the variable 'age'.
-//  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
+// // example: export const {value1} = myObject;
 
-const user = { name: "John", years: 30 };
+// // EXERCISE 2
+// // We have an object called 'user'.
+// // Write the destructuring assignment that reads:
+// //  2.1: 'name' property into the variable 'name'.
+// //  2.2: 'years' property into the variable 'age'.
+// //  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
 
-// EXERCISE 3
-// Extract the variables from the dog object and
-// rename them like "dogName", "dogBreed", and "dogAge", respectively.
+// const user = { nameOfPerson: "John", years: 30, isAdmin: true };
 
-const dog = {
-  name: "Pluto",
-  breed: "husky",
-  age: 5,
-};
+// // const { nameOfPerson, years: age } = user;
+
+// // const { years: age } = user;
+
+// // const { nameOfPerson, years: age, isAdmin = false } = user;
+
+// // console.log(nameOfPerson, age, isAdmin);
+// // const { name, years } = user;
+// // EXERCISE 3
+// // Extract the variables from the dog object and
+// // rename them like "dogName", "dogBreed", and "dogAge", respectively.
+
+// const dog = {
+//   name: "Pluto",
+//   breed: "husky",
+//   age: 5,
+// };
+
+// const { name: dogName, breed: dogBreed, age: dogAge } = dog;
 
 // EXERCISE 4
 // Extract the 'lastName' property from the person object as "personLastName".
@@ -40,3 +53,7 @@ const person = {
   age: 24,
   firstName: "Alex",
 };
+
+const { lastName: personLastName, ...moreInformation } = person;
+
+console.log(personLastName, moreInformation);
