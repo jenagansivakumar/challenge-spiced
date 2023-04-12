a7c635a03e4eb71a434d56f9c13d9be75d40d009
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-04-12 15:30:21 +0200

    finished

--- react/react-nesting/buttons-and-children/react-nesting_buttons-and-children/src/App.js
+++ react/react-nesting/buttons-and-children/react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button> test 1 </Button>
+      <Button> test 2</Button>
+      <Button> test 3 </Button>
+      <Button> test 4 </Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
