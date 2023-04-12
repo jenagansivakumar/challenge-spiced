24bb561fc3998ef14031f43b550e3b8279fec436
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-04-12 10:47:59 +0200

    finished first challenge (I think)

--- react/react-props/greeting/react-props_greeting/src/App.js
+++ react/react-props/greeting/react-props_greeting/src/App.js
@@ -1,5 +1,16 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting />;
 }
+
+const name = Jena;
+function Greeting({ name }) {
+  return <h1>Hello {name}</h1>;
+}
+
+// - Write the `Greeting` component inside of the [`App.js`](src/App.js).
+//   - It accepts a prop called `name` (make sure to destructure it).
+//   - It returns an HTML element and uses the `name` prop to render "Hello, [name]!";
+// - Inside of the return statement of your `App` component, replace the heading with your `Greeting` component and pass it a `name` prop with a value of your choice.
+``;
