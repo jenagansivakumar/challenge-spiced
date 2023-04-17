b7356eb3035f263b3a5543fc44d720fc6f2d539c
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-04-17 11:56:31 +0200

    finished

---
+++ react/react-arrays/fruits/react-with-arrays_fruits/src/App.js
@@ -0,0 +1,43 @@
+import Card from "./components/Card";
+import { Fragment } from "react";
+
+export default function App() {
+  const fruits = [
+    { id: 0, name: "banana", colour: "yellow" },
+    { id: 1, name: "orange", colour: "orange" },
+    { id: 2, name: "strawberry", colour: "red" },
+    { id: 3, name: "kiwi", colour: "brown" },
+    { id: 4, name: "grape", colour: "purple" },
+  ];
+
+  return (
+    
+      <div className="app">
+        {fruits.map((fruit, index) => (
+          <Card
+            key={index}
+            name={fruit.name}
+            colour={fruit.colour}
+            id={fruit.id}
+          />
+        ))}
+      </div>
+
+  );
+}
+
+// return (
+//   <div className="app">
+//     <div>
+//       {fruits.map(({ id, name, colour }) => (
+//         <h2>
+//           <Card key={id}>
+//             <h1>
+//               {name} {colour}
+//             </h1>
+//           </Card>{" "}
+//         </h2>
+//       ))}
+//     </div>
+//   </div>
+// );
