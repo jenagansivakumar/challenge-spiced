8074e3d1d00c66a10484865dffd2e94487c1348c
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2023-05-04 15:03:26 +0200

    finished

--- backend/backend-basics_chance/server.js
+++ backend/backend-basics_chance/server.js
@@ -1,13 +1,16 @@
 import { createServer } from "node:http";
 import Chance from "chance";
+import React from "react";
 
 const chance = new Chance();
 
 export const server = createServer((request, response) => {
-  const name = chance.name();
+  const name = chance.name({ prefix: true });
   const age = chance.age();
   const profession = chance.profession();
+  const animal = chance.animal();
+
   response.end(
-    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}. I also love ${animal}s`
   );
 });
